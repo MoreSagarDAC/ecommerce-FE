f40a0c8d7489af0ec4ceeb50f9a904842218e8f9
sagar.more <sagar.more@instinctinnovations.com>
2025-12-20 19:31:45 +0530

    SM : Enhance routing in App component with login and registration pages; update .gitignore to include .env file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import Button from "./framework/Button";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isLoginPage = location.pathname === "/login";
+
+  const handleNavigation = () => {
+    navigate(isLoginPage ? "/register" : "/login");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px" }}
+      >
+        <Button
+          label={isLoginPage ? "Switch to Register" : "Switch to Login"}
+          onClick={handleNavigation}
+          color="violet"
+          sx={{ maxWidth: "200px" }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div style={{ marginTop: "10px", marginBottom: "10px" }}>
-          <Button label="Test Me Out" onClick={() => alert("Button clicked!")} />
-        </div>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
@@ -6,6 +7,8 @@ import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
